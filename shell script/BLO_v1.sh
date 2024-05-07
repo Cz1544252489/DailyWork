9ab54bc4d5166946a821e1b2ad83a3a7c80e113f
ウメとタク <64407693+Cz1544252489@users.noreply.github.com>
2024-05-07 09:11:25 +0800

    Update BLO_v1.sh

--- shell script/BLO_v1.sh
+++ shell script/BLO_v1.sh
@@ -140,7 +140,7 @@ sed -i "1iimport warnings" ~/BOML/test_script/script_helper.py
 sed -i "2iwarnings.filterwarnings('ignore',category=FutureWarning)" ~/BOML/test_script/script_helper.py
 
 # 后一行用消除来自tensorflow的警告
-sed -i "5itf.get_logger().setLevel('ERROR')" ~/BOML/boml/extension.py
+sed -i "30itf.get_logger().setLevel('ERROR')" ~/BOML/boml/extension.py
 
 }
 
